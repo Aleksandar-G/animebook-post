7deb2ef7843aef476af80a35e2543a0d3a66efcb
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-03-21 17:19:05 +0100

    chore(demo-post.js): add post seeder

---
+++ seeders/20220321153728-demo-post.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Posts', [{
+      user_id: 1,
+      content: "test post 1",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      user_id: 1,
+      content: "test post 2",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('Posts', null, {});
+  }
+};
