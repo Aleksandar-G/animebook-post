98edb06a07bbc5b75a9fabc8f0514d07a7c1c3ef
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-03-21 17:33:22 +0100

    feat(post.js): post router with find and post request

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,32 @@
 const express = require("express")
+const { Post } = require("../models/");
 const router = express.Router()
 
+// fetch posts per user
 router.get('/:userId', (req, res) => {
-    res.send(req.params.userId)
+
+
+    Post.findAll({
+        where: {
+            user_id: req.params.userId
+        }
+    }).then((posts) => {
+        res.send(posts)
+        console.log(posts);
+    })
+
 })
 
+// create a new post
 router.post('/', (req, res) => {
-    res.send("post")
+    console.log(req.body.user_id);
+    console.log(req.body.content);
+    const user_id = req.body.user_id
+    const content = req.body.content
+    Post.build({ user_id, content }).save().then(() => {
+        res.status(201)
+        res.send("saved",)
+    })
 })
 
 module.exports = router
