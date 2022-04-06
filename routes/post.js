4ff714fdee49730e48aa4a7446ba2e3c1fd9d30f
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-04-06 20:56:33 +0200

    feat(post.js): add token verification

--- routes/post.js
+++ routes/post.js
@@ -1,27 +1,57 @@
 const express = require("express")
 const router = express.Router()
 const { postPerUser, createPost } = require("../controllers/postController")
+const rabbitmq = require('../services/rabbitmq')
+const { verifyJWT } = require('../services/verifyJWT')
+
+let channel;
+
+rabbitmq.rabbitMQChannel()
+    .then((ch) => {
+        channel = ch
+    })
 
 // fetch posts per user
 router.get('/:userId', (req, res) => {
 
-    const posts = postPerUser(req.params.userId)
+    const rpcMessage = { "token": req.headers.authorization, "username": req.body.username }
 
-    res.send(posts)
+    rabbitmq.sendRPCRequest(channel, rpcMessage, rabbitmq.verifyQueue)
+        .then((verified) => {
 
+            if (!verified) {
+                res.status(401)
+                res.send("not authenticated")
+            } else {
+                const posts = postPerUser(req.params.userId)
+
+                res.send(posts)
+            }
+        })
 })
 
 // create a new post
 router.post('/', (req, res) => {
-    const user_id = req.body.user_id
-    const content = req.body.content
-    const saved = createPost(user_id, content)
-    if (!saved) {
-        res.status(500)
-    } else {
-        res.status(201)
-        res.send("saved",)
-    }
+
+
+    const rpcMessage = { "token": req.headers.authorization, "username": req.body.username }
+
+    rabbitmq.sendRPCRequest(channel, rpcMessage, rabbitmq.verifyQueue).then((verified) => {
+        if (!verified) {
+            res.status(401)
+            res.send('not authenticated')
+        } else {
+            const user_id = req.body.user_id
+            const content = req.body.content
+            const saved = createPost(user_id, content)
+            if (!saved) {
+                res.status(500)
+            } else {
+                res.status(201)
+                res.send("saved",)
+            }
+        }
+    })
 })
 
 module.exports = router
