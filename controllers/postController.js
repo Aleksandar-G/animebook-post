6a657234f2fa5aac52fea1f745d2d88d92654ea6
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-05-08 20:53:11 +0200

    refactor(postController.js): lint the code

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,48 +1,56 @@
-const {postPerUser, createPost} = require('../services/postService')
-const rabbitmq = require('../services/rabbitmq')
-const { verifyJWT } = require('../services/verifyJWT')
+const {
+  postPerUser,
+  createPost,
+  allPosts,
+} = require("../services/postService");
+const rabbitmq = require("../services/rabbitmq");
+const { verifyJWT } = require("../services/verifyJWT");
 
 let channel;
 
-rabbitmq.rabbitMQChannel()
-    .then((ch) => {
-        channel = ch
-    })
+rabbitmq.rabbitMQChannel().then((ch) => {
+  channel = ch;
+});
 
+const getPostsPerUser = async (userId, JWTtoken) => {
+  //const verifiedUser = await verifyJWT(channel,rpcMessage)
+  const verifiedUser = true;
+  if (!verified) {
+    throw new Error("not authenticated");
+  } else {
+    const posts = await postPerUser(userId);
+    //console.log(posts);
+    return posts;
+  }
+};
 
-const getPostsPerUser = async (userId,rpcMessage) => {
+const savePost = async (content, JWTtoken) => {
+  const rpcMessage = JWTtoken.toString();
+  const verifiedUser = await verifyJWT(channel, rpcMessage);
+  //const verifiedUser = true;
 
-    //const verified = await verifyJWT(channel,rpcMessage)
-    const verified = true
-        if (!verified) {
-            throw new Error("not authenticated")
-        }else{
-            const posts = await postPerUser(userId)
-            console.log(posts);
-            return posts
-        }
+  if (verifiedUser === "") {
+    throw new Error("not authenticated");
+  } else {
+    if (!content) throw new Error("no content");
+    const created = await createPost(verifiedUser.userId, content);
 
+    return created;
+  }
+};
 
-}
+const getAllPosts = async (JWTtoken, offset) => {
+  const verifiedUser = await verifyJWT(channel, JWTtoken);
+  //const verified = true;
+  if (verifiedUser === "") {
+    throw new Error("not authenticated");
+  } else {
+    const posts = await allPosts(offset);
+    //console.log(posts);
+    return posts;
+  }
+};
 
-const savePost = async (userId, content, rpcMessage) => {
-
-    //const verified = await verifyJWT(channel,rpcMessage)
-    const verified = true
-
-    if (!verified) {
-        throw new Error("not authenticated")
-    } else {
-        
-        if (!content) throw new Error("no content")
-        console.log(userId);
-        const created = await createPost(userId,content)
-
-        return created
-       
-    }   
-}
-
-exports.savePost = savePost
-exports.getPostsPerUser = getPostsPerUser
-
+exports.savePost = savePost;
+exports.getPostsPerUser = getPostsPerUser;
+exports.getAllPosts = getAllPosts;
